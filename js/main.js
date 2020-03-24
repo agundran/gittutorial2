ed788b43675280710503bc51c3dcdc8626b14d25
Arnold G <agundran@gmail.com>
2020-03-24 13:16:11 +0800

    learning console.log in JS

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//arrays - variables that holds multiple values
+
+/* multi line comment
+*/
+
+const fruits =['apples','oranges','pears'];  //array constructors
+
+fruits[3] ='grapes'; //adding 4th array value
+
+fruits.push('mangoes');  // pushing a value to the last array
+fruits.unshift('strawberries');  //pushing value first of the array
+fruits.pop();
+
+
+console.log(fruits);
+console.log(Array.isArray('apples'));
+console.log(fruits.indexOf('oranges'));
+
+
---
+++ js/main2.js
@@ -0,0 +1,18 @@
+const person ={
+    firstName: 'John',
+    lastName: 'Doe',
+    age:30,
+    hobbies: ['Collecting Coins','playing guitar', 'singing'],
+    address:{
+        steet: '50 main st.',
+        city:'Boston',
+        state: 'MA'
+    }
+
+}
+
+console.log(person.hobbies[1]);
+console.log(person.address.city);
+
+const {firstName, lastName, address: {city}} = person;
+console.log(city);
